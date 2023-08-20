c36f78a7978aad709da7004890d48e33ce90c2fd
Rootamiunik <142379304+Rootamiunik@users.noreply.github.com>
2023-08-20 22:43:30 +0545

    comment update

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
+#------------Url-and-website-----------------#
 URL = "https://www.empireonline.com/movies/features/best-movies-2/"
 html_data = requests.get(url=URL).text
 soup = BeautifulSoup(html_data,'html.parser')
 data = soup.find_all(name='h3',class_='listicleItem_listicle-item__title__hW_Kn')
 
+#------------Screapper-----------------#
 for i in data[::-1]:
     with open('movie.txt',"a") as file:
         file.write(f"{i.getText()}\n")
