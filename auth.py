83fd79122ad71d18b74513a507918a43b5e3c665
Rootamiunik <142379304+Rootamiunik@users.noreply.github.com>
2023-08-22 20:05:52 +0545

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,20 +3,22 @@ from dotenv import load_dotenv
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import ast
+import requests
 
 load_dotenv()
 
 CLIENT_ID = os.getenv("id")
 CLIENT_SECRET = os.getenv("key")
 REDIRECT_URL = 'http://example.com'
+ENDPOINT = 'https://api.spotify.com.'
 
 #---------------------Brain-of-operation------------#
 class Brain:
     def __init__(self) -> None:
-        try:
-            os.remove(".cache")
-        except FileNotFoundError:
-            pass
+        # try:
+        #     os.remove(".cache")
+        # except FileNotFoundError:
+        #     pass
         self.sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                     client_secret=CLIENT_SECRET,
                                                     redirect_uri="http://example.com",
@@ -29,8 +31,9 @@ class Brain:
         self.header = {
             'Authorization': f'Bearer {self.auth}' 
         }
-        
 
+        server_req = requests.get(url=ENDPOINT,headers=self.header,timeout=120)
+        print(server_req)
               
             
                 
