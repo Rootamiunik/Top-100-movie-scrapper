3d425eaa6487a0dfa0ac5534cdab82d7c748fd95
Rootamiunik <142379304+Rootamiunik@users.noreply.github.com>
2023-08-23 19:14:32 +0545

    Restoring previous commit.

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-import os
-from dotenv import load_dotenv
-from auth import Auth
-from brain import Brain
-import ast
-import spotipy
+import requests
+from bs4 import BeautifulSoup
 
-#--------------main-----------------#
+URL = "https://www.empireonline.com/movies/features/best-movies-2/"
+html_data = requests.get(url=URL).text
+soup = BeautifulSoup(html_data,'html.parser')
+data = soup.find_all(name='h3',class_='listicleItem_listicle-item__title__hW_Kn')
 
+for i in data[::-1]:
+    with open('movie.txt',"a") as file:
+        file.write(f"{i.getText()}\n")
 
-date = input("Playlist of Top 100 songs of date (YYYY-MM-DD):  ")
-
-brain = Brain(date)
-brain.create_playlist() #creates playlist
-brain.add_songs() #adds song
